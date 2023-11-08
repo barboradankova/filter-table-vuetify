71f2f6d4e4e447c826b7c0918cd16acb1c852613
Barbora Dankova <barbora.dankova@geovap.cz>
2023-11-08 15:27:49 +0100

    Loaded data from json

--- src/App.vue
+++ src/App.vue
@@ -10,101 +10,42 @@
 </template>
 <script setup>
     import { ref } from 'vue'
-
     import TableFilter from './components/TableFilter.vue';
+    import CP from '../data/places.json'
 
-    const headers = ref([
-        { title: 'Dessert (100g serving)', align: 'start', sortable: false, key: 'name', type: 'text'},
-        { title: 'Calories', align: 'end', key: 'calories', type: 'numeric'},
-        { title: 'Fat (g)', align: 'end', key: 'fat', type: 'numeric' },
-        { title: 'Carbs (g)', align: 'end', key: 'carbs', type: 'numeric' },
-        { title: 'Protein (g)', align: 'end', key: 'protein', type: 'numeric' },
-        { title: 'Iron (%)', align: 'end', key: 'iron', type: 'numeric' },
-      ])
+    const camelToFlat = ((camel) => {
+      const camelCase =camel.replace(/([a-z])([A-Z])/g, '$1 $2').split(" ")
 
-    const data = ref([
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22',
-          },
-      ])
+      let flat =""
 
+      camelCase.forEach(word=>{    
+          flat = flat + word.charAt(0).toUpperCase() + word.slice(1) + " "
+      })  
+      return flat
+    })
 
+    const setWidth = ((item) => {
+      if (typeof CP.pageItems[0][item] == 'number'){
+        return `${item.length*1.5}rem`
+      } 
+      
+      if (!CP.pageItems[0][item] || CP.pageItems[0][item] == 'none'){
+        return `${item.length*1.1}rem`
+      }
+
+      return `${item.length*2.5}rem`
+    })
+
+    const headers = ref(Object.keys(CP.pageItems[0]).map((item, index) => {
+      return {
+        title: camelToFlat(item),
+        key: item,
+        type: typeof CP.pageItems[0][item] == 'number' ? 'numeric' : 'text',
+        width: setWidth(item),
+        align: typeof CP.pageItems[0][item] == 'number' ? 'end' : 'start'
+      }
+    }))
+
+    const data = ref(CP.pageItems)
 
 </script>
